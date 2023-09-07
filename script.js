44982242f1c3c9823d3f97c0a4443159b5b595bb
Johan <jjohan357@gmail.com>
2023-09-07 13:20:16 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let intro = document.querySelector('.intro');
+let logo = document.querySelector('.logo-header');
+let logoSpan = document.querySelectorAll('.logo');
+
+window.addEventListener('DOMContentLoaded', ()=>{
+   setTimeout(()=>{
+    intro.style.top = '-100vh';
+   }, 2500)
+
+
+
+
+
+
+
+
+
+
+
+})
